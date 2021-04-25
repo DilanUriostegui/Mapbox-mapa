76fa440fb15cf1cb8c564c8a326c24102656f761
Dilan <duriostegui@asgbpm.com>
2021-04-24 21:23:10 -0500

    Implementación de Mapbox funcionando con fullscreen

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
-
-class FullScreenMap extends StatelessWidget {
+import 'package:mapbox_gl/mapbox_gl.dart';
+ 
+class FullScreenMap extends StatefulWidget {
+  @override
+  _FullScreenMapState createState() => _FullScreenMapState();
+}
+ 
+class _FullScreenMapState extends State<FullScreenMap> {
+  static const String ACCESS_TOKEN = 'pk.eyJ1IjoiZGlsYW51cmlvc3RlZ3VpIiwiYSI6ImNrbndpdnhuYTBkeTgybnFkMXp4N2p2Y2sifQ.S23JZjYtQ4NaYOLrNmqBFQ';
+ 
+  MapboxMapController mapController;
+ 
+  void _onMapCreated(MapboxMapController controller){
+    mapController = controller;
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
-      ),
-      
+      body: MapboxMap(
+        accessToken: ACCESS_TOKEN,
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(19.59256727290991, -99.1671063254336),
+          zoom: 16.0
+        ),
+      )
     );
   }
 }
