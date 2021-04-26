7e243e09977a29c5f9faa241a3182054b5c304fd
Dilan <duriostegui@asgbpm.com>
2021-04-26 11:53:15 -0500

    Se implementaron los botones de zoom in, zoom out y los marcadores desde assets o url

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
  
 class FullScreenMap extends StatefulWidget {
@@ -19,29 +24,92 @@ class _FullScreenMapState extends State<FullScreenMap> {
 
   void _onMapCreated(MapboxMapController controller){
     mapController = controller;
+    _onStyleLoaded();
+  }
+
+   void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/custom-icon.png");
+    addImageFromUrl("networkImage", Uri.parse("https://via.placeholder.com/50"));
+  }
+
+
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController.addImage(name, list);
+  }
+
+
+  Future<void> addImageFromUrl(String name, Uri uri) async {
+    var response = await http.get(uri);
+    return mapController.addImage(name, response.bodyBytes);
   }
  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: crearMapa(),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            child: Icon(Icons.add_to_home_screen),
-            onPressed: (){
-              if(selectedStyle == oscuroStyle){
-                selectedStyle = streetStyle;
-              }
-              else{
-                selectedStyle = oscuroStyle;
-              }
-              setState(() {});
+      floatingActionButton: botonesFlotantes(),
+    );
+  }
+
+  Column botonesFlotantes() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+
+
+        //Simbolos
+        FloatingActionButton(
+          child: Icon(Icons.sentiment_very_dissatisfied),
+          onPressed: (){
+            mapController.addSymbol(SymbolOptions(
+              geometry: center,
+              //iconSize: 3,
+              iconImage: 'networkImage',
+              textField: 'Montaña creada aqui',
+              textOffset: Offset(0, 2)
+            ));
+          }
+        ),
+
+        SizedBox(height: 5),
+
+
+        //Zoom in
+        FloatingActionButton(
+          child: Icon(Icons.zoom_in),
+          onPressed: (){
+            mapController.animateCamera( CameraUpdate.zoomIn() );
+          }
+        ),
+
+        SizedBox(height: 5),
+
+        //Zoom out
+        FloatingActionButton(
+          child: Icon(Icons.zoom_out),
+          onPressed: (){
+            mapController.animateCamera( CameraUpdate.zoomOut() );
+          }
+        ),
+
+
+        //Cambiar estilo
+        FloatingActionButton(
+          child: Icon(Icons.add_to_home_screen),
+          onPressed: (){
+            if(selectedStyle == oscuroStyle){
+              selectedStyle = streetStyle;
             }
-          )
-        ]
-      ),
+            else{
+              selectedStyle = oscuroStyle;
+            }
+            _onStyleLoaded();
+            setState(() {});
+          }
+        )
+      ]
     );
   }
 
