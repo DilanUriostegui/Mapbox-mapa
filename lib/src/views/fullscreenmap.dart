decdc22174275752154f8a562a509d30af99181e
Dilan <duriostegui@asgbpm.com>
2021-04-26 11:29:51 -0500

    Configuración de estilos con un boton

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -10,7 +10,13 @@ class _FullScreenMapState extends State<FullScreenMap> {
   static const String ACCESS_TOKEN = 'pk.eyJ1IjoiZGlsYW51cmlvc3RlZ3VpIiwiYSI6ImNrbndpdnhuYTBkeTgybnFkMXp4N2p2Y2sifQ.S23JZjYtQ4NaYOLrNmqBFQ';
  
   MapboxMapController mapController;
- 
+
+  final center = LatLng(19.59256727290991, -99.1671063254336);
+  final oscuroStyle = 'mapbox://styles/dilanuriostegui/cknysyi1a48l917rh22kiapk0';
+  final streetStyle = 'mapbox://styles/dilanuriostegui/cknyt0r9r2yke17qensclt3cp';
+  String selectedStyle = 'mapbox://styles/dilanuriostegui/cknysyi1a48l917rh22kiapk0';
+
+
   void _onMapCreated(MapboxMapController controller){
     mapController = controller;
   }
@@ -18,14 +24,36 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-        accessToken: ACCESS_TOKEN,
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(19.59256727290991, -99.1671063254336),
-          zoom: 16.0
-        ),
-      )
+      body: crearMapa(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.add_to_home_screen),
+            onPressed: (){
+              if(selectedStyle == oscuroStyle){
+                selectedStyle = streetStyle;
+              }
+              else{
+                selectedStyle = oscuroStyle;
+              }
+              setState(() {});
+            }
+          )
+        ]
+      ),
+    );
+  }
+
+  MapboxMap crearMapa() {
+    return MapboxMap(
+      styleString: selectedStyle,
+      accessToken: ACCESS_TOKEN,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: center,
+        zoom: 16.0
+      ),
     );
   }
 }
